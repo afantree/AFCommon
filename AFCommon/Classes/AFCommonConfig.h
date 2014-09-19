761deb4b745fde8e4a4d32c6179b0ef133494714
阿凡树 <729397005@qq.com>
2014-09-19 11:52:33 +0800

    update config

--- AFCommon/Classes/AFCommonConfig.h
+++ AFCommon/Classes/AFCommonConfig.h
@@ -9,6 +9,7 @@
 #ifndef AFCommon_AFCommonConfig_h
 #define AFCommon_AFCommonConfig_h
 
+#pragma mark -- 程序沙盒文件路径
 // 程序目录，不能存任何东西
 #define APP_HOME_PATH NSHomeDirectory()
 // 文档目录，需要ITUNES同步备份的数据存这里
@@ -21,5 +22,14 @@
 #define APP_CACHES_PATH [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0]
 // 缓存目录，APP退出后，系统可能会删除这里的内容
 #define APP_TEMP_PATH NSTemporaryDirectory()
+
+#pragma mark -- 系统和APP的信息
+//系统版本号
+#define SYSTEM_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
+//当前应用版本 版本比较用
+#define APP_CURRENT_VERSION [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
+//当前应用名字
+#define APP_CURRENT_NAME [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"]
+
 
 #endif
